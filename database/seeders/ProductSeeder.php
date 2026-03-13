3aa47b0ae8a50ea6d3d506045ab770b5c81dadf7
Csizmadi Gyöngyike <csizmadigyongyike@elg-bp.edu.hu>
2026-03-13 13:00:29 +0100

    Adatok feltöltése

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -50,6 +50,87 @@ class ProductSeeder extends Seeder
                 "updated_at" => now()
             ],
             [
+                "name" => "Logitech G915 Lightspeed",
+                "category_id" => 1,
+                "description" => "",
+                "price" => 22990,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Redragon S113",
+                "category_id" => 1,
+                "description" => "",
+                "price" => 23499,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Keychron K2 V2 Aluminum",
+                "category_id" => 1,
+                "description" => "",
+                "price" => 25990,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Logitech Wireless Combo MK270",
+                "category_id" => 1,
+                "description" => "",
+                "price" => 21990,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Corsair K65 RGB MINI",
+                "category_id" => 1,
+                "description" => "",
+                "price" => 22499,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Asus ROG Strix Flare II Animate",
+                "category_id" => 1,
+                "description" => "",
+                "price" => 23999,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Razer BlackWidow V3",
+                "category_id" => 1,
+                "description" => "",
+                "price" => 23499,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Razer DeathStalker V2 Pro",
+                "category_id" => 1,
+                "description" => "",
+                "price" => 26999,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Corsair K55 RGB PRO XT",
+                "category_id" => 1,
+                "description" => "",
+                "price" => 24499,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
                 "name" => "BlazeDragon Inferno KB",
                 "category_id" => 2,
                 "description" => "A BlazeDragon Inferno FX gamer egér tökéletes társa: a BlazeDragon Inferno KB mechanikus
@@ -58,6 +139,96 @@ class ProductSeeder extends Seeder
                         tartós switch-ek pedig villámgyors reakciót biztosítanak. A masszív, prémium kivitel hosszú
                         éveken át bírja az intenzív játékot.",
                 "price" => 32990,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "SteelSeries Sensei Ten",
+                "category_id" => 2,
+                "description" => "",
+                "price" => 34999,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Cooler Master MM720 Glossy",
+                "category_id" => 2,
+                "description" => "",
+                "price" => 35499,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "HP HyperX Pulsefire Haste",
+                "category_id" => 2,
+                "description" => "",
+                "price" => 29999,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "MSI CLUTCH GM30",
+                "category_id" => 2,
+                "description" => "",
+                "price" => 31499,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Asus TUF Gaming M4 Wireless",
+                "category_id" => 2,
+                "description" => "",
+                "price" => 29999,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Asus ROG GLADIUS II ORIGIN PNK LTD",
+                "category_id" => 2,
+                "description" => "",
+                "price" => 32599,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Asus ROG Gladius II Origin",
+                "category_id" => 2,
+                "description" => "",
+                "price" => 37699,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Razer DeathAdder",
+                "category_id" => 2,
+                "description" => "",
+                "price" => 38499,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Asus P705 ROG PUGIO II",
+                "category_id" => 2,
+                "description" => "",
+                "price" => 37899,
+                "stock" => 0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Pulsar X2 Mini",
+                "category_id" => 2,
+                "description" => "",
+                "price" => 35499,
                 "stock" => 0,
                 "created_at" => now(),
                 "updated_at" => now()
