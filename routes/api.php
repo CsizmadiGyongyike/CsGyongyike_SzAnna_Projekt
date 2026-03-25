d8a4347332b3eb6ad0147813f253c82fddc2c283
Szkaliczka Anna <szkaliczkaanna03@gmail.com>
2026-03-25 10:18:59 +0100

    Felesleges kódrészletek kiszedés

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/products', [ProductController::class, 'index']);
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{product}', [ProductController::class, 'update']);
